3e301c95a99410106f40cdc4962fd84d4a535b62
Longphi Nguyen <nguyenl707@gmail.com>
2017-05-20 16:25:11 -0700

    Added indicators for car wheels on edge of track

---
+++ Tutorial - 2D Car Racing/Assets/Scripts/Car2dController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Car2dController : MonoBehaviour {
+
+	float speedForce = 15f;
+	float torqueForce = -200f;
+	float driftFactorSticky = 0.9f;
+	float driftFactorSlippy = 1;
+	float maxStickyVelocity = 2.5f;
+	//float minSlippyVelocity = 1.5f;	// <--- Exercise for the viewer
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update() {
+		// check for button up/down here, then set a bool that you will use in FixedUpdate
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		//Debug.Log(RightVelocity().magnitude);
+
+		float driftFactor = driftFactorSticky;
+		if(RightVelocity().magnitude > maxStickyVelocity) {
+			driftFactor = driftFactorSlippy;
+		}
+
+		rb.velocity = ForwardVelocity() + RightVelocity()*driftFactor;
+
+		if(Input.GetButton("Accelerate")) {
+			rb.AddForce( transform.up * speedForce );
+
+			// Consider using rb.AddForceAtPosition to apply force twice, at the position
+			// of the rear tires/tyres
+		}
+		if(Input.GetButton("Brakes")) {
+			rb.AddForce( transform.up * -speedForce/2f );
+            
+			// Consider using rb.AddForceAtPosition to apply force twice, at the position
+			// of the rear tires/tyres
+		}
+
+		// If you are using positional wheels in your physics, then you probably
+		// instead of adding angular momentum or torque, you'll instead want
+		// to add left/right Force at the position of the two front tire/types
+		// proportional to your current forward speed (you are converting some
+		// forward speed into sideway force)
+		float tf = Mathf.Lerp(0, torqueForce, rb.velocity.magnitude / 2);
+		rb.angularVelocity = Input.GetAxis("Horizontal") * tf;
+
+
+
+	}
+
+	Vector2 ForwardVelocity() {
+		return transform.up * Vector2.Dot( GetComponent<Rigidbody2D>().velocity, transform.up );
+	}
+
+	Vector2 RightVelocity() {
+		return transform.right * Vector2.Dot( GetComponent<Rigidbody2D>().velocity, transform.right );
+	}
+
+    /*
+    void polyVertCycle(PolygonCollider2D col, SpriteRenderer r)
+    {
+        Vector2[] points = col.points;
+
+        Vector2 shortestpoint = new Vector2();
+        double shortestDistance = 999f;
+
+        for(int i = 0; i<points.Length; i++)
+        {
+            double currentDist = Math.Pow(points[i].x, 2) + Math.Pow(points[i].y, 2);
+
+            if(currentDist < shortestDistance)
+            {
+                shortestDistance = currentDist;
+                shortestpoint = points[i];
+                Debug.Log(Math.Pow(points[i].x, 2) + Math.Pow(points[i].y, 2));
+            }
+        }
+
+        r.transform.position = shortestpoint;
+    }
+    */
+}
